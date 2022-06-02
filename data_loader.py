17a6181477a8f40f83a175ec8bf0cee65fd563b2
Ahmed Moubtahij <ahmed.moubtahij@croesus.com>
2022-06-01 21:15:10 -0400

    Read paragraphs and removed empty paragraph: "[w:sdt]"

--- data_loader.py
+++ data_loader.py
@@ -1,15 +1,18 @@
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import argparse
 from pathlib import Path
+from typing import Iterator, List, Any
 import docx
 from simplify_docx import simplify
 
+# TODO: write the DataLoader class then import it in data_analysis.ipynb
 
 def main(args) -> None:
     data_path = make_data_path(args.data_dir, args.data_fn)
 
-    my_doc_as_json: dict = simplify(docx.Document(data_path))
-    print(my_doc_as_json)
+    data = DataLoader(data_path)
+    print(data)
+
 
 def make_data_path(data_dir: str, data_fn: str) -> Path:
     data_path = Path(data_dir + data_fn).expanduser().resolve()
@@ -26,6 +29,24 @@ def get_args() -> argparse.Namespace:
                             default="D5627-Dolan.docx",
                             help=("File name with the text to summarize"))
     return arg_parser.parse_args()
+
+
+class DataLoader:
+
+    def __init__(self, data_path: Path) -> None:
+        assert data_path.exists()
+        self.raw_doc: dict = simplify(docx.Document(data_path))
+        self.doc: List[dict] = self.raw_doc["VALUE"][0]["VALUE"]
+
+        self.paragraphs: Iterator[List[dict]] = self.__class__._init_paragraphs(self.doc)
+
+    @staticmethod
+    def _init_paragraphs(doc: List[dict]) -> Iterator[List[dict]]:
+        _paragraphs: Iterator[Any] = map(lambda p: p["VALUE"], doc)
+        # https://github.com/microsoft/Simplify-Docx/blob/
+        # ce493b60e3e4308bde7399257426c0c68a6c699b/src/
+        # simplify_docx/iterators/body.py#L60
+        return (p for p in _paragraphs if p != "[w:sdt]")
 
 
 if __name__ == "__main__":
