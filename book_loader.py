4813975edafe8cb69d10bb844cbeff3246447dbe
Ahmed Moubtahij <ahmed.moubtahij@croesus.com>
2022-06-18 17:24:21 -0400

    Refac of

--- book_loader.py
+++ book_loader.py
@@ -3,15 +3,14 @@ import argparse
 from pathlib import Path
 from typing import Any, Iterator, List, Union, Dict, Callable
 import re
-from itertools import chain, groupby, islice
+from itertools import chain, dropwhile, groupby
 import docx
 from simplify_docx import simplify
 
-# TODO: Import `BookLoader` into `data_analysis.ipynb`
 
 def main(args) -> None:
     data_path = make_data_path(args.data_dir, args.data_fn)
-    book = BookLoader(data_path, tables_of_contents_idx=152)
+    book = BookLoader(data_path)
     print(book.chapters)
 
 
@@ -33,19 +32,31 @@ def get_args() -> argparse.Namespace:
 
 class BookLoader:
 
-    def __init__(self, data_path: Path,
-                 re_separator=r"^Chapitre (\d+) \/$",
-                 tables_of_contents_idx=0):
+    # pylint: disable=E0602 # undefined-variable
+    Table = List[dict]
+    TextOrTable = Union[str, Table]
+
+    def __init__(self,
+                 data_path: Path,
+                 title="",
+                 intro_marker=r"^Introduction$",
+                 chapter_separator=r"^Chapitre (\d+) \/$",
+                 header_marker=""):
         assert data_path.exists()
         self.docx: dict = simplify(docx.Document(data_path))
-        self.tables_of_contents_idx = tables_of_contents_idx
+
         self.paragraphs = self._init_paragraphs()
-        self.re_separator = re_separator
+        self.title = next(self.paragraphs) if not title else title
+        self.intro_marker = intro_marker
+        self.chapter_separator = chapter_separator
+        self.header_marker = (header_marker if header_marker
+                              else (re.sub(r"\(|\)", '', self.chapter_separator)
+                                    .removesuffix('$')
+                                    + r".*"))
         self.chapters = self._init_chapters()
 
-    # TODO: Look into aliasing `[Union[str, List[dict]]]` as text_or_tables
-    def _chapter_indexer(self) -> Callable[[Union[str, List[dict]]], int]:
-        separator_pattern = re.compile(self.re_separator)
+    def _chapter_indexer(self) -> Callable[[TextOrTable], int]:
+        separator_pattern = re.compile(self.chapter_separator)
         current_chapter = 0
 
         def indexer(paragraph: Union[str, List[dict]]) -> int:
@@ -57,21 +68,26 @@ class BookLoader:
 
         return indexer
 
-    # TODO: Look into aliasing `[Union[str, List[dict]]]` as text_or_tables
-    def _strip_headers(self, paragraphs: Iterator[Union[str, List[dict]]]) -> List[Union[str, List[dict]]]:
-        page_headers = ["Stress, santé et performance au travail", "Introduction"]
-        header_pattern = re.sub(r"\(|\)", '', self.re_separator).removesuffix('$') + r".*"
-        return [p for p in paragraphs
-                if isinstance(p, str)
-                and p not in page_headers
-                and not re.match(header_pattern, p)]
+    def _strip_headers(self, paragraphs: Iterator[TextOrTable]) -> Iterator[TextOrTable]:
+        return (p for p in paragraphs
+                if not isinstance(p, str) # not str => Table => not a header
+                or (p != self.title
+                    and not re.match(self.intro_marker, p)
+                    and not re.match(self.header_marker, p)))
 
-    def _init_chapters(self) -> Dict[int, List[Union[str, List[dict]]]]:
-        _chapters = groupby(self.paragraphs, self._chapter_indexer())
-        chapters = {idx: self._strip_headers(paragraphs) for idx, paragraphs in _chapters}
+
+    def _init_chapters(self) -> Dict[int, List[TextOrTable]]:
+        paragraphs_from_intro = dropwhile(
+            lambda p: (not isinstance(p, str)
+                       or not re.match(self.intro_marker, p)),
+            self.paragraphs)
+
+        _chapters = groupby(paragraphs_from_intro, self._chapter_indexer())
+        chapters = {idx: list(self._strip_headers(paragraphs))
+                    for idx, paragraphs in _chapters}
         return chapters
 
-    def _init_paragraphs(self) -> Iterator[Union[str, List[dict]]]:
+    def _init_paragraphs(self) -> Iterator[TextOrTable]:
         _docx: List[dict] = self.docx["VALUE"][0]["VALUE"]
 
         _paragraphs: Iterator[Any]
@@ -80,7 +96,6 @@ class BookLoader:
         _paragraphs = chain.from_iterable(_paragraphs)
         _paragraphs = filter(lambda p: p["TYPE"] != "CT_Empty", _paragraphs)
         _paragraphs = map(lambda p: p["VALUE"], _paragraphs)
-        _paragraphs = islice(_paragraphs, self.tables_of_contents_idx, None)
 
         return _paragraphs
 
