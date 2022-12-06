de2af1422c52821e2099eb08e48131f5374ca2d7
Ayenem <moub.ahmed@hotmail.com>
2022-12-05 20:06:37 -0500

    rm unnecessities

--- summaries_reader.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import jsonlines as jsonl
-
-
-summaries_fp = Path("data/output_summaries/textrank_french_semantic_summaries.jsonl").resolve()
-with jsonl.open(summaries_fp, mode='r') as summaries_f:
-    summaries = list(summaries_f)
-
-print()
